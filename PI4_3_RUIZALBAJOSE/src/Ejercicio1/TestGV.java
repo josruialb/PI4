72fc6519c7f2fcde896da981d2b7847e514c354a
pepe1 <pepe1@DESKTOP-Q4OJ2C4>
2019-05-29 23:48:44 +0200

    grafos virtuales

--- PI4_3_RUIZALBAJOSE/src/Ejercicio1/TestGV.java
+++ PI4_3_RUIZALBAJOSE/src/Ejercicio1/TestGV.java
@@ -1,36 +1,36 @@
 package Ejercicio1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiFunction;
 
 import us.lsi.astar.AStarAlgorithm;
+import us.lsi.astar.AStarGraph;
+import us.lsi.astar.AStarSimpleVirtualGraph;
+import us.lsi.astar.PredicateHeuristic;
 import us.lsi.graphs.SimpleEdge;
 
 public class TestGV {
 
 	public static void main(String[] args) {
+		List<Integer> ls = Arrays.asList(1, 2, 3, 4, 0, 5, 6, 7, 8);
+		VerticeSuma estadoInicial = VerticeSuma.create(ls, 0, 0, 0, new ArrayList<>());
 
-//		VerticeSuma estadoInicial = VerticeSuma.create(0, 1, 2, 3, 4, 5, 6, 7, 8);
-//		VerticeSuma estadoFinal = VerticeSuma.create(1, 2, 3, 4, 0, 5, 6, 7, 8);
-//
-//		System.out.println("Estado inicial\n" + estadoInicial);
-//		System.out.println("Estado final\n" + estadoFinal);
-//
-//		var grafo = GrafoSuma.create(estadoInicial, estadoFinal);
-//		BiFunction<VerticeSuma, VerticeSuma, Double> heuristica = (e1, e2) -> e1.getNumDiferentes(e2) - 1.; 
-//		// es -1 porque aunque cambiemos 2 elementos, hacemos un solo movimiento
-//
-//		var alg = AStarAlgorithm.of(grafo, estadoInicial, estadoFinal, heuristica);
-//
-//		System.out.println("Vértices del camino mínimo:\n");
-//		alg.getPathVertexList().forEach(v -> System.out.println(v + "\n"));
-//
-//		System.out.println("Número de movimientos:");
-//		System.out.println(alg.getPath().getLength());
-//
-//		System.out.println("Vértices vecinos del estado inicial:");
-//		var ss = grafo.edgesOf(estadoInicial);
-//		for (SimpleEdge<VerticeSuma> simpleEdge : ss) {
-//			System.out.println(Graphs.getOppositeVertex(grafo, simpleEdge, estadoInicial));
-//		}
+		AStarGraph<VerticeSuma, SimpleEdge<VerticeSuma>> grafoEstrella = AStarSimpleVirtualGraph
+				.of(v -> VerticeSuma.pesoArista(v));
+
+		PredicateHeuristic<VerticeSuma> heuristica = (e1, e2) -> e1.getTam();
+		// es -1 porque aunque cambiemos 2 elementos, hacemos un solo movimiento
+
+		System.out.println("Vértices del camino mínimo:\n");
+
+		AStarAlgorithm<VerticeSuma, SimpleEdge<VerticeSuma>> alg = AStarAlgorithm.of(grafoEstrella, estadoInicial, v-> VerticeSuma.esEstadoFinal(v), heuristica);
+
+
+		VerticeSuma v = alg.getPathVertexList().get(alg.getPathVertexList().size() - 1);
+
+		System.out.println(v);
+
 	}
 }
--- PI4_3_RUIZALBAJOSE/src/Ejercicio1/VerticeSuma.java
+++ PI4_3_RUIZALBAJOSE/src/Ejercicio1/VerticeSuma.java
@@ -1,28 +1,133 @@
 package Ejercicio1;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import us.lsi.graphs.SimpleEdge;
 import us.lsi.graphs.VirtualVertex;
 
-public class VerticeSuma implements VirtualVertex<VerticeSuma, SimpleEdge<VerticeSuma>> {{
+public class VerticeSuma implements VirtualVertex<VerticeSuma, SimpleEdge<VerticeSuma>> {
 
+	private List<Integer> num;
+	private Integer sumIzq;
+	private Integer sumDer;
+	private Integer pos;
+	private List<Integer> listSumIzq;
+
+	public VerticeSuma(List<Integer> num, Integer sumIzq, Integer sumDer, Integer pos, List<Integer> listSumIzq) {
+		super();
+		this.num = num;
+		this.sumIzq = sumIzq;
+		this.sumDer = sumDer;
+		this.pos = pos;
+		this.listSumIzq = listSumIzq;
+	}
+
+	public static VerticeSuma create(List<Integer> num, Integer sumIzq, Integer sumDer, Integer pos,
+			List<Integer> listSumIzq) {
+		return new VerticeSuma(num, sumIzq, sumDer, pos, new ArrayList<Integer>());
+	}
+
+	@Override
+	public boolean isValid() {
+		// TODO Auto-generated method stub
+		return this.pos<= num.size();
+	}
+
+	@Override
+	public Set<VerticeSuma> getNeighborListOf() {
+		
+		Set<VerticeSuma> s = new HashSet<VerticeSuma>();
+		if(this.pos==this.num.size()) {
+			return s;
+		}
+		List<Integer> nListSumIzq= new ArrayList<Integer>(this.listSumIzq);
+		nListSumIzq.add(this.num.get(this.pos));
+		
+		s.add(new VerticeSuma(num, sumIzq+this.num.get(this.pos), sumDer, pos+1, nListSumIzq));
+		nListSumIzq = new ArrayList<Integer>(this.listSumIzq);
+		s.add(new VerticeSuma(num, sumIzq, this.sumDer + this.num.get(this.pos) , pos+1, nListSumIzq));
+		
+		return s;
+	}
+
+	@Override
+	public Set<SimpleEdge<VerticeSuma>> edgesOf() {
+		// TODO Auto-generated method stub
+		return this.getNeighborListOf().stream().map(x->SimpleEdge.of(this,x)).collect(Collectors.toSet());
+	}
+
+public static Boolean esEstadoFinal(VerticeSuma v) {
+	return v.pos== v.num.size() && v.sumIzq==v.sumDer;
+}
+public static Double pesoArista(SimpleEdge<VerticeSuma> v ) {
+	VerticeSuma origen = v.source;
+	VerticeSuma destino = v.target;
+	Double res = 0.;
+	if(origen.sumIzq<destino.sumDer) {
+		res =1.;
+	}
+return res;
+}
+public Double getTam() {
+	return (double) this.listSumIzq.size();
 }
 
 @Override
-public boolean isValid() {
-	// TODO Auto-generated method stub
-	return false;
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((listSumIzq == null) ? 0 : listSumIzq.hashCode());
+	result = prime * result + ((num == null) ? 0 : num.hashCode());
+	result = prime * result + ((pos == null) ? 0 : pos.hashCode());
+	result = prime * result + ((sumDer == null) ? 0 : sumDer.hashCode());
+	result = prime * result + ((sumIzq == null) ? 0 : sumIzq.hashCode());
+	return result;
 }
 
 @Override
-public Set<VerticeSuma> getNeighborListOf() {
-	// TODO Auto-generated method stub
-	return null;
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	VerticeSuma other = (VerticeSuma) obj;
+	if (listSumIzq == null) {
+		if (other.listSumIzq != null)
+			return false;
+	} else if (!listSumIzq.equals(other.listSumIzq))
+		return false;
+	if (num == null) {
+		if (other.num != null)
+			return false;
+	} else if (!num.equals(other.num))
+		return false;
+	if (pos == null) {
+		if (other.pos != null)
+			return false;
+	} else if (!pos.equals(other.pos))
+		return false;
+	if (sumDer == null) {
+		if (other.sumDer != null)
+			return false;
+	} else if (!sumDer.equals(other.sumDer))
+		return false;
+	if (sumIzq == null) {
+		if (other.sumIzq != null)
+			return false;
+	} else if (!sumIzq.equals(other.sumIzq))
+		return false;
+	return true;
 }
 
 @Override
-public Set<SimpleEdge<VerticeSuma>> edgesOf() {
-	// TODO Auto-generated method stub
-	return null;
-}}
+public String toString() {
+	return "VerticeSuma [listSumIzq=" + listSumIzq + "]";
+}
+
+}
